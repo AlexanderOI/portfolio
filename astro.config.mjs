7d9e60d7f8e4381a1b5b670d1ae3af02427e8ac3
AlexanderOI <vialexoriba1257@gmail.com>
2024-10-23 22:33:19 -0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://alexander-oi.vercel.app",
+  integrations: [tailwind(), sitemap()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
